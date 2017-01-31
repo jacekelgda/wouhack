831d3f33046b325078b69544d7dd5733d744e7f8
Jacek Lawniczak <jacekelgda@gmail.com>
2017-01-31 15:47:13 +0100

    Heroku port listner

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var express = require('express')
 var app = express()
 
+const version = '0.0.1'
+
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
+var port = process.env.PORT || 8080
+app.listen(port, function() {
+  console.log('listening on port ' + port)
+  console.log('version: ' + version)
 })
